4f9ba8906cb38b79cffc65f7cfbfaa672447fffd
Jeremiah F. Alfred <jeremiahalfred56@gmail.com>
2025-10-07 18:09:59 +0000

    Delete gtbank_app directory

--- gtbank_app/android/app/src/main/kotlin/com/example/gtbank_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gtbank_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- gtbank_app/lib/screens/messages.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Messages page
-class MessagesPage extends StatelessWidget {
-  const MessagesPage({super.key});
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text("💬 Messages"));
-}
